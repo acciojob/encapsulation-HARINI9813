e11a313826c807998ea5067608dba63ea508dee4
HARINI-RAJAN <harinirajan8184@gmail.com>
2024-02-08 00:15:08 +0530

    submitting solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void Main(String[] args)
+    {
+        RWOnly obj=new RWOnly();
+        //obj.name="Haarini";
+        //'name' has private access in 'com.driver.RWOnly'
+        obj.setName("Harini");
+        System.out.println(obj.getName());
+    }
 }
